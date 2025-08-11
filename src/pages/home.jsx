4b25c1f5252561613dd3428490f7cdc38753233c
huza <huzaifahasif3@gmail.com>
2025-08-11 23:04:40 +0700

    update: route kepsek.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import Hero from '../components/hero'
 import Prestasi from '../components/cardPrestasi'
 import Slogan from '../components/sloganSekolah'
 import ToggleTheme from '../components/toggleTheme';
+import Kepsek from '../components/kepsek';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Slogan />
       <Prestasi />
       <ToggleTheme />
+      <Kepsek />
     </div>
   )
 }
