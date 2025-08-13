e6a9b72e301f409baafd4a14b5d6cd72ba6f2eca
huza <huzaifahasif3@gmail.com>
2025-08-13 17:31:35 +0700

    refine galleryGrid & make it a bit responsive

--- src/components/galleryGrid.jsx
+++ src/components/galleryGrid.jsx
@@ -15,17 +15,28 @@ export default function GalleryGrid() {
         style={{ backgroundImage: `url(${bgImage})` }}
       ></div>
 
-      {/* card */}
-      <div className="relative grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {images.map((src, i) => (
-          <img
-            key={i}
-            src={src}
-            alt=""
-            loading="lazy"
-            className="w-full h-64 rounded-lg object-cover"
-          />
-        ))}
+      {/* konten */}
+      <div className="relative">
+        {/* grid gambar */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mb-8">
+          {images.map((src, i) => (
+            <img
+              key={i}
+              src={src}
+              alt=""
+              loading="lazy"
+              className="w-full h-64 rounded-lg object-cover"
+            />
+          ))}
+        </div>
+
+        {/* tombol view more */}
+        <div className="absolute left-1/2 -translate-x-1/2">
+          <button className="py-4 px-6 bg-blue-500 text-white font-semibold rounded-full hover:bg-white/30 transition">
+            View More
+          </button>
+        </div>
+
       </div>
     </section>
   );
